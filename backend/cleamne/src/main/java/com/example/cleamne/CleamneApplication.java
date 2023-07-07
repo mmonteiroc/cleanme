05902d1b9377766868f17e7736c8acff7a3cf355
miguel monteiro <miguelmonteiroclaveri@gmail.com>
2023-07-07 21:43:36 +0200

    init project

---
+++ backend/cleamne/src/main/java/com/example/cleamne/CleamneApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleamne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleamneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleamneApplication.class, args);
+    }
+
+}
---
+++ backend/cleamne/src/test/java/com/example/cleamne/CleamneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleamne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleamneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
